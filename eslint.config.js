5bb769732ac405e954de5ed5d8239f6fdcf8900e
François Roland <francois.roland@heh.be>
2025-03-10 14:08:33 +0000

    Tune settings.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  "rules": {
-    "no-console": 0,
-    "eqeqeq": "warn",
-    "no-cond-assign": 0,
-    "no-unused-vars": 1,
-    "no-extra-semi": "warn",
-    "semi": "warn"
-  }
+  rules: {
+    'no-console': 0,
+    eqeqeq: 'warn',
+    'no-cond-assign': 0,
+    'no-unused-vars': 1,
+    'no-extra-semi': 'warn',
+    quotes: ['single'],
+    semi: 'warn',
+  },
 };
---
+++ scripts.js
@@ -0,0 +1,5 @@
+'use strict';
+
+$(() => {
+  $('#message').text('Hello, World!');
+});
